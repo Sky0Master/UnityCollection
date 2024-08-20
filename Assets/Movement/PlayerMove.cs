2d2ea38c58e7fc436f0cf8f0e2de34e49dcce170
Vino <sky0master@163.com>
2024-08-20 12:09:54 +0800

    2dplatform playermove

--- Assets/Movement/PlayerMove.cs
+++ Assets/Movement/PlayerMove.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.Collections;
 using UnityEngine;
 
 
@@ -10,62 +8,77 @@ using UnityEngine;
 //     //xxxxxxx
 // }
 
+
+// 0:idle   1:
+public enum PlayerState
+{
+    Idle = 0,
+    Walk = 1,
+    Jump = 2,
+}
+
 public class PlayerMove : MonoBehaviour
 {
-
-    SpriteRenderer spr;
     Rigidbody2D rb; 
     public float Speed;
     public float FallSpeed = 0.5f;
     public bool isGrounded;
+    public bool isMove;
     public float JumpForce = 10f;
-    private int _hp;
-    public int Hp{
-        get => _hp;
-        set{
-            if(value<0) value = 0;
-            if(value==0)
-                Die();
-            _hp = value;
-        }
-    }
-    public GameObject enemy;
-    public void Die()
-    {
-
-    }
-
-    public void SetEventHandler(GameObject enem)
-    {
-        enem.GetComponent<Enemy>().DeathEvent += OnEnemyDie;
-    }
-
+    public float timeScale = 1f;
+    public float groundCheckDistance = 0.5f;
+    public Transform checkPoint1;
+    public Transform checkPoint2;
+    Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-      
+        animator = GetComponent<Animator>();
+        
+
     }
-    private void OnCollisionEnter2D(Collision2D other) {
+/*    private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "Blocking")
         {
             isGrounded = true;
+            animator.SetBool("grounded", true);
         }
     }
-
-    public void OnEnemyDie(object sender,GameObject target)
-    {
-        Debug.Log($"Enemy {target.name} Die!");
-    }
-
     private void OnCollisionExit2D(Collision2D other) {
         if(other.gameObject.tag == "Blocking")
         {
             isGrounded = false;
+            animator.SetBool("grounded", true);
+        }
+    }*/
+    private void OnDrawGizmos()
+    {
+        if(checkPoint1)
+            Debug.DrawLine(checkPoint1.position, checkPoint1.position + Vector3.down * groundCheckDistance);
+        if(checkPoint2)
+            Debug.DrawLine(checkPoint2.position, checkPoint2.position + Vector3.down * groundCheckDistance);
+    }
+    public void GroundCheck()
+    {
+        var res1 = Physics2D.Raycast(checkPoint1.position,Vector2.down, groundCheckDistance,LayerMask.GetMask("Ground"));
+        var res2 = Physics2D.Raycast(checkPoint2.position, Vector2.down, groundCheckDistance,LayerMask.GetMask("Ground"));
+
+        if((res1.collider!=null) || (res2.collider!=null))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
         }
     }
-    
+    private void FixedUpdate() {
+        GroundCheck();
+    }
     void Update()
     {
+        
+        Time.timeScale = timeScale;
         float xv = 0;
         if(Input.GetKey(KeyCode.D))
         {
@@ -77,16 +90,32 @@ public class PlayerMove : MonoBehaviour
         }
         
         float dvy = 0;
+        
         if(isGrounded)
         {
+            
             if(Input.GetKeyDown(KeyCode.Space))
             {
                 rb.AddForce(new Vector2(0,JumpForce), ForceMode2D.Impulse);
+                animator.SetTrigger("jump");
             }
         }
         else{
             dvy = -FallSpeed * Time.deltaTime;
         }
+       
+        var scaleValue = Mathf.Abs(transform.localScale.x);
+        if(xv > 0.01f)
+        {
+            transform.localScale = new Vector3(scaleValue,scaleValue,0);
+        }
+        if(xv<-0.01f)
+        {
+            transform.localScale = new Vector3(-scaleValue, scaleValue, 0);
+        }
+        isMove = Mathf.Abs(xv) > 0.01f;
+        animator.SetBool("is_move", isMove);
+        animator.SetBool("grounded", isGrounded);
         rb.velocity = new Vector2(xv, rb.velocity.y + dvy);
     }
 }
